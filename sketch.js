2e48963d5d8972138522ae75cc8b9bc4ba889264
Domeoryx <79259699+Domeoryx@users.noreply.github.com>
2021-04-24 13:41:45 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,12 +13,12 @@ var car1_img, car2_img, car3_img, car4_img, track,ground;
 
 
 function preload(){
-car1_img = loadImage("../images/car1.png");
-car2_img= loadImage("../images/car2.png");
-car3_img= loadImage("../images/car3.png");
-car4_img= loadImage("../images/car4.png");
-track  =loadImage("../images/track.jpg");
-ground = loadImage("../images/ground.png");
+car1_img = loadImage("images/car1.png");
+car2_img= loadImage("images/car2.png");
+car3_img= loadImage("images/car3.png");
+car4_img= loadImage("images/car4.png");
+track  =loadImage("images/track.jpg");
+ground = loadImage("images/ground.png");
 }
 
 
